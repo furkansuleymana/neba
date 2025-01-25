82b3d6fc0b7e657dee6a32ce1b7855112cfb6f85
Furkan <furkansuleymanalici@gmail.com>
2025-01-25 18:26:44 +0100

    test SSDP device discovery

--- cmd/neba/main.go
+++ cmd/neba/main.go
@@ -2,36 +2,44 @@ package main
 
 import (
 	"log"
+	"log/slog"
 	"net/http"
 	"os"
 
 	"github.com/furkansuleymana/neba/configs"
+	"github.com/furkansuleymana/neba/discovery"
 	"github.com/furkansuleymana/neba/handlers"
 	"github.com/furkansuleymana/neba/ui"
 )
 
 func main() {
-	// create configuration manager
+	// Create configuration manager
 	cm, err := configs.ConfigManager()
 	if err != nil {
 		os.Exit(1)
 	}
 
-	// get current config
+	// Get current config
 	config := cm.Get()
 
-	// setup routes
+	// Setup routes
 	mux := http.NewServeMux()
 	handlers.RegisterFactoryDefaultRoute(mux)
 	handlers.RegisterRestartRoute(mux)
 
-	// serve ui
+	// Serve UI
 	server := http.FileServer(http.FS(ui.DistDirFS))
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		server.ServeHTTP(w, r)
-
 	})
 
-	// listen
+	// TESTING
+	deviceList, _ := discovery.DiscoverWithSSDP()
+	for _, device := range deviceList {
+		slog.Info("Found device", slog.String("device", device))
+	}
+	// TESTING
+
+	// Go!
 	log.Fatal(http.ListenAndServe(config.Server.HTTP.Port, mux))
 }
