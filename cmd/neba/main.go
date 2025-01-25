7ac7a872aaba35d5d4e190eefa61f48fdba01447
Furkan <furkansuleymanalici@gmail.com>
2025-01-25 04:37:57 +0100

    removed config test

--- cmd/neba/main.go
+++ cmd/neba/main.go
@@ -1,55 +1,37 @@
 package main
 
 import (
-	"io/fs"
 	"log"
-	"log/slog"
 	"net/http"
 	"os"
 
-	"github.com/furkansuleymana/neba/settings"
+	"github.com/furkansuleymana/neba/configs"
+	"github.com/furkansuleymana/neba/handlers"
 	"github.com/furkansuleymana/neba/ui"
 )
 
 func main() {
-	// create settings manager
-	cm, err := settings.SettingsManager()
+	// create configuration manager
+	cm, err := configs.ConfigManager()
 	if err != nil {
 		os.Exit(1)
 	}
 
-	// get current settings
-	setting := cm.Get()
-	slog.Info("retrieved settings", slog.Any("settings", setting))
+	// get current config
+	config := cm.Get()
 
-	// update settings
-	err = cm.Update(func(set *settings.ApplicationSettings) {
-		set.Server.HTTP.Address = "127.0.0.1"
-		set.Server.HTTP.Port = "8080"
-		set.Database.Path = "/new/path/to/database"
+	// setup routes
+	mux := http.NewServeMux()
+	handlers.RegisterFactoryDefaultRoute(mux)
+	handlers.RegisterRestartRoute(mux)
+
+	// serve ui
+	server := http.FileServer(http.FS(ui.DistDirFS))
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		server.ServeHTTP(w, r)
+
 	})
-	if err != nil {
-		slog.Error("failed to update settings", "error", err)
-	}
 
-	// get updated settings
-	setting = cm.Get()
-	slog.Info("retrieved settings", slog.Any("settings", setting))
-
-	//
-
-	fileSystem, err := fs.Sub(ui.Dist, "dist")
-	if err != nil {
-		log.Fatalf("failed to create filesystem: %v", err)
-	}
-
-	http.Handle("/", http.FileServer(http.FS(fileSystem)))
-
-	addr := ":8080"
-	log.Printf("starting server on %s\n", addr)
-
-	if err := http.ListenAndServe(addr, nil); err != nil {
-		log.Fatalf("failed to start server: %v", err)
-	}
-
+	// listen
+	log.Fatal(http.ListenAndServe(config.Server.HTTP.Port, mux))
 }
