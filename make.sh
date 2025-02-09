0eab943f74ac45942e5d69b7108f47ebf9eec7c4
Furkan <furkansuleymanalici@gmail.com>
2025-02-09 18:04:16 +0100

    feat: add make.sh script for build and development tasks

---
+++ make.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+set -e
+
+# Define variables
+ROOT_DIR="$(pwd)"
+BUILD_DIR_GO="${ROOT_DIR}/build"
+BUILD_DIR_NPM="${ROOT_DIR}/ui"
+BINARY_NAME="neba"
+
+# Set environment variables
+export GOOS=windows
+export GOARCH=amd64
+export CGO_ENABLED=0
+LDFLAGS="-s"
+
+# Build project for production
+build() {
+  clean
+  tidy
+  npm --prefix "$BUILD_DIR_NPM" run build
+  go build -ldflags="${LDFLAGS}" -o "${BUILD_DIR_GO}/${BINARY_NAME}" "${ROOT_DIR}"
+}
+
+# Run project in development mode
+dev() {
+  go run "${ROOT_DIR}"
+  npm --prefix "$BUILD_DIR_NPM" run dev
+}
+
+# Tidy project
+tidy() {
+  go mod tidy -v
+  go fmt ./...
+  npm --prefix "$BUILD_DIR_NPM" run lint
+}
+
+# Clean build directories
+clean() {
+  rm -rf "$BUILD_DIR_GO"
+  rm -rf "$BUILD_DIR_NPM/dist"
+  rm -rf "$BUILD_DIR_NPM/node_modules"
+  npm --prefix "$BUILD_DIR_NPM" run clean
+}
+
+# Get task name (default to "dev") and
+# execute task if valid, else print error
+TASK_NAME="${1:-dev}"
+if ! declare -F "$TASK_NAME" >/dev/null; then
+  echo "Invalid task name: $TASK_NAME" >&2
+  exit 1
+fi
+$TASK_NAME
