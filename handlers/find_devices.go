8d128578abf1b707602b3c09c6d276623e38652b
Furkan <furkansuleymanalici@gmail.com>
2025-02-13 23:04:58 +0100

    feat: add API route for finding devices with SSDP discovery

---
+++ handlers/find_devices.go
@@ -0,0 +1,41 @@
+package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/furkansuleymana/neba/network"
+)
+
+func RegisterFindDevicesRoute(mux *http.ServeMux) {
+	mux.HandleFunc("/api/find_devices", handleFindDevices)
+}
+
+func handleFindDevices(w http.ResponseWriter, req *http.Request) {
+	if req.Method != http.MethodGet {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	devices, err := findDevices()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	if err := json.NewEncoder(w).Encode(devices); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func findDevices() ([]string, error) {
+	deviceList, err := network.DiscoverSSDP()
+	if err != nil {
+		return nil, err
+	}
+	return deviceList, nil
+}
