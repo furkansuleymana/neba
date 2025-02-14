3a0c5650c21e098a873021fc7dcc6683a24baba0
Furkan <furkansuleymanalici@gmail.com>
2025-02-14 01:20:24 +0100

    lint

--- main.go
+++ main.go
@@ -2,13 +2,13 @@ package main
 
 import (
 	"log"
+	"log/slog"
 	"net/http"
 	"os"
 
 	"github.com/furkansuleymana/neba/configs"
-	"github.com/furkansuleymana/neba/database"
-	"github.com/furkansuleymana/neba/database/models"
 	"github.com/furkansuleymana/neba/handlers"
+	"github.com/furkansuleymana/neba/network"
 	"github.com/furkansuleymana/neba/ui"
 )
 
@@ -25,6 +25,7 @@ func main() {
 	// Setup routes
 	mux := http.NewServeMux()
 	handlers.RegisterFactoryDefaultRoute(mux)
+	handlers.RegisterFindDevicesRoute(mux)
 	handlers.RegisterRestartRoute(mux)
 
 	// Serve UI
@@ -34,30 +35,8 @@ func main() {
 	})
 
 	// TESTING
-	device := models.AxisDevice{
-		SerialNumber: "A12B34C56D78",
-		IPAddress:    "192.168.1.1",
-		Model:        "M1075-L",
-		OSVersion:    "11.2.68",
-	}
-	bucketName := "devices"
-
-	db, err := database.Open(config.Database.Path, bucketName)
-	if err != nil {
-		log.Fatalf("Could not initialize database: %v", err)
-	}
-	defer database.CloseDB(db)
-
-	if err := database.Update(db, bucketName, device); err != nil {
-		log.Fatalf("Could not save device: %v", err)
-	}
-
-	retrievedDevice, err := database.View(db, bucketName, "A12B34C56D78")
-	if err != nil {
-		log.Fatalf("Could not get device: %v", err)
-	}
-
-	log.Println(retrievedDevice)
+	deviceList, _ := network.DiscoverSSDP()
+	slog.Info("", slog.Any("devices", deviceList))
 	os.Exit(0)
 	// TESTING
 
