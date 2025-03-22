a091914785ffffe18f26754db672b75b1fcac404
Furkan <furkansuleymanalici@gmail.com>
2025-03-22 19:52:20 +0100

    refactor: simplify build and development commands in make.sh

--- make.sh
+++ make.sh
@@ -5,7 +5,6 @@ set -e
 # Define variables
 ROOT_DIR="$(pwd)"
 BUILD_DIR_GO="${ROOT_DIR}/build"
-BUILD_DIR_NPM="${ROOT_DIR}/ui"
 BINARY_NAME="neba"
 
 # Set environment variables
@@ -18,15 +17,12 @@ LDFLAGS="-s"
 build() {
   clean
   tidy
-  run "$BUILD_DIR_NPM" "npm install"
-  run "$BUILD_DIR_NPM" "npm run build"
   go build -ldflags="${LDFLAGS}" -o "${BUILD_DIR_GO}/${BINARY_NAME}" "${ROOT_DIR}"
 }
 
 # Run project in development mode
 dev() {
-  go run "${ROOT_DIR}" &
-  run "$BUILD_DIR_NPM" "npm run dev"
+  go build -o "${BUILD_DIR_GO}/${BINARY_NAME}" "${ROOT_DIR}" && "${BUILD_DIR_GO}/${BINARY_NAME}"
 }
 
 # Tidy project
@@ -38,14 +34,6 @@ tidy() {
 # Clean build directories
 clean() {
   rm -rf "$BUILD_DIR_GO"
-  rm -rf "$BUILD_DIR_NPM/dist"
-  rm -rf "$BUILD_DIR_NPM/node_modules"
-}
-
-# Run command in directory
-run() {
-  cd "$1"
-  eval "$2"
 }
 
 # Get task name (default to "dev") and
